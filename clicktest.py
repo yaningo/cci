9121548bba7de931415b3a5aebd8e5802e73d0a1
yaningo <yann.domingo@gmail.com>
2020-01-02 21:33:35 +0000

    Update test script

--- clicktest.py
+++ clicktest.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
 
 #driver = webdriver.Firefox()
 caps = DesiredCapabilities.FIREFOX.copy()
