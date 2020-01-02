c8c3b99ac7ef5a492aa756f7ff61742e993a2c44
yaningo <yann.domingo@gmail.com>
2020-01-02 21:30:50 +0000

    Update test script

--- clicktest.py
+++ clicktest.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Firefox()
+#driver = webdriver.Firefox()
+caps = DesiredCapabilities.FIREFOX.copy()
+caps['marionette'] = False
+driver=webdriver.Firefox(capabilities=caps)
 driver.get("file:///C:/temp/personal/GitHub/index.html")
 driver.execute_script("document.getElementById('clickety').click()")
 
