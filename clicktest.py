803dbd72d58d02712c491e205d1f5c35d4f134c1
yaningo <yann.domingo@gmail.com>
2020-01-02 23:22:21 +0000

    Add dir_path variable

--- clicktest.py
+++ clicktest.py
@@ -1,13 +1,17 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
-#driver = webdriver.Firefox()
+dir_path = os.path.dirname(os.path.realpath(__file__))
+path_index = os.path.join(dir_path, 'index.html')
+
+driver = webdriver.Firefox()
 caps = DesiredCapabilities.FIREFOX.copy()
-caps['marionette'] = False
+#caps['marionette'] = False
 driver=webdriver.Firefox(capabilities=caps)
-driver.get("file:///index.html")
+driver.get("file:///" + path_index)
 driver.execute_script("document.getElementById('clickety').click()")
 
 assert "Happy New Year :-)" in driver.page_source
